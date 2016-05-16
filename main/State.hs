af995749ab7ef6e33c7e368ffdf6c6411741b22f
Markus Hauck <markus1189@gmail.com>
2016-05-16 11:07:42 +0200

    Function to view all items at once

--- main/State.hs
+++ main/State.hs
@@ -8,23 +8,26 @@ module State (HocketState
              ,focusRing
              ,hsNumItems
              ,hsLastUpdated
-             ,hsContents
              ,itemListName
              ,pendingListName
 
              ,initialState
              ,addItemsUnread
+             ,contentsView
              ) where
 
 import           Brick (Name)
 import qualified Brick.Focus as F
 import qualified Brick.Widgets.List as L
 import           Control.Lens
+import           Data.Foldable (foldl')
 import           Data.Function (on)
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.Maybe (fromMaybe)
+import           Data.Monoid ((<>))
 import           Data.Time.Clock.POSIX (POSIXTime)
+import           Data.Vector (Vector)
 import qualified Data.Vector as V
 
 import           Widgets
@@ -34,7 +37,6 @@ data HocketState = HocketState { _itemListVi :: ViList PocketItem
                                , _pendingListVi :: ViList PocketItem
                                , _focusRing :: F.FocusRing
                                , _hsLastUpdated :: Maybe POSIXTime
-                               , _hsContents :: Map PocketItemId [PocketItem]
                                }
 makeLenses ''HocketState
 
@@ -47,7 +49,6 @@ initialState = HocketState (ViList $ L.list itemListName V.empty 1)
                            (ViList $ L.list pendingListName V.empty 1)
                            (F.focusRing [itemListName, pendingListName])
                            Nothing
-                           Map.empty
 
 itemListName :: Name
 itemListName = "items"
@@ -58,7 +59,8 @@ pendingListName = "pending"
 itemList :: Lens' HocketState (L.List PocketItem)
 itemList = itemListVi . _ViList
 
-addItemsUnread :: (Functor f, Foldable f) => POSIXTime -> f PocketItem -> HocketState -> HocketState
+addItemsUnread :: (Functor f, Foldable f)
+               => POSIXTime -> f PocketItem -> HocketState -> HocketState
 addItemsUnread ts pis s =
   s & itemList %~ applyAll (fmap (listInsertSorted (view timeUpdated)) pis)
     & itemList . L.listElementsL %~ V.reverse
@@ -76,3 +78,9 @@ listInsertSorted toOrd x lxs = L.listInsert insertPos x lxs
         insertPos = fromMaybe (length xs)
                               (V.findIndex (((<) `on`) toOrd x) xs)
         xs = L.listElements lxs
+
+contentsView :: HocketState -> Map PocketItemId (Vector PocketItem)
+contentsView hs = buildMap $ extract itemList hs <> extract pendingList hs
+  where extract l = view (l . L.listElementsL)
+        buildMap = foldl' go Map.empty
+        go acc item = Map.insertWith (<>) (item ^. itemId) (V.singleton item) acc
