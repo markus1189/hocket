5986ebe137795f637a179cd74ea0321a5c438219
Markus Hauck <markus1189@gmail.com>
2016-05-16 15:18:17 +0200

    Functions to modify state as if it was only one map

--- main/State.hs
+++ main/State.hs
@@ -14,18 +14,20 @@ module State (HocketState
              ,initialState
              ,addItemsUnread
              ,contentsView
+             ,fromContentsView
+             ,withContents
              ) where
 
 import           Brick (Name)
 import qualified Brick.Focus as F
 import qualified Brick.Widgets.List as L
 import           Control.Lens
-import           Data.Foldable (foldl')
+import           Data.Foldable (foldl',toList)
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.Monoid ((<>))
+import qualified Data.Set as Set
 import           Data.Time.Clock.POSIX (POSIXTime)
-import           Data.Vector (Vector)
 import qualified Data.Vector as V
 
 import           Widgets
@@ -70,8 +72,22 @@ pendingList = pendingListVi . _ViList
 applyAll :: Foldable f => f (a -> a) -> a -> a
 applyAll fs z = foldl (&) z fs
 
-contentsView :: HocketState -> Map PocketItemId (Vector PocketItem)
-contentsView hs = buildMap $ extract itemList hs <> extract pendingList hs
+contentsView :: HocketState -> Map PocketItemId PocketItem
+contentsView hs = buildMap (extract itemList hs <> extract pendingList hs)
   where extract l = view (l . L.listElementsL)
         buildMap = foldl' go Map.empty
-        go acc item = Map.insertWith (<>) (item ^. itemId) (V.singleton item) acc
+        go acc item = Map.insert (item ^. itemId) item acc
+
+fromContentsView :: Map PocketItemId PocketItem -> HocketState -> HocketState
+fromContentsView contents s = s & itemList . L.listElementsL .~ V.fromList newItems
+                                & pendingList . L.listElementsL .~ V.fromList newPendings
+  where oldItemIds = Set.fromList (toListOf (itemList.L.listElementsL.each.itemId) s)
+        oldPendingIds = Set.fromList (toListOf (pendingList.L.listElementsL.each.itemId) s)
+        newItems = toList $ Map.filterWithKey (knownFrom oldItemIds) contents
+        newPendings = toList $ Map.filterWithKey (knownFrom oldPendingIds) contents
+        knownFrom frm pid _ = Set.member pid frm
+
+withContents :: (Map PocketItemId PocketItem -> Map PocketItemId PocketItem)
+             -> HocketState
+             -> HocketState
+withContents f s = fromContentsView (f (contentsView s)) s
