15dfce908814e78734d1b1e300ccd554507dfadb
Markus Hauck <markus1189@gmail.com>
2016-08-20 15:43:35 +0200

    Remove dead code from test

--- test/test.hs
+++ test/test.hs
@@ -18,20 +18,6 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
--- properties :: TestTree
--- properties = testGroup "Properties" [scProps, qcProps]
-
--- -- qcProps = testGroup "(checked by QuickCheck)"
---   [ QC.testProperty "sort == sort . reverse" $
---       \list -> sort (list :: [Int]) == sort (reverse list)
---   , QC.testProperty "Fermat's little theorem" $
---       \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
---   -- the following property does not hold
---   , QC.testProperty "Fermat's last theorem" $
---       \x y z n ->
---         (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
---   ]
-
 pocketItem1 :: PocketItem
 pocketItem1 = PocketItem "excerpt"
                          True
