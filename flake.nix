ebcf79690b0b0ce93538f9b1dc5dc3d6748476d2
Markus Hauck <markus1189@gmail.com>
2022-05-29 09:53:10 +0200

    Add default package and adapt workflow

--- flake.nix
+++ flake.nix
@@ -8,9 +8,14 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         hocketPkg = pkgs.haskellPackages.callCabal2nix "hocket" ./. { };
+        drv = pkgs.haskell.lib.justStaticExecutables hocketPkg;
       in rec {
-        apps.hocket = pkgs.haskell.lib.justStaticExecutables hocketPkg;
+        apps.hocket = pkgs.haskell.lib.justStaticExecutables drv;
         defaultApp = apps.hocket;
+
+        packages.hocket = drv;
+        defaultPackage = packages.hocket;
+
         devShell = pkgs.haskellPackages.developPackage {
           root = ./.;
           modifier = with pkgs.haskell.lib;
