152f42287453c6da7646da55fe1764622e240850
Markus Hauck <markus1189@gmail.com>
2023-01-04 13:42:20 +0100

    Formatting

--- src/Network/Pocket/Meta.hs
+++ src/Network/Pocket/Meta.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Network.Pocket.Meta where
 
-import Data.Aeson.Lens
-import Control.Lens ((^?))
-import Formatting (string, sformat, (%))
-import Network.Wreq (get, responseBody, )
+import           Control.Lens ((^?))
+import           Data.Aeson.Lens
+import           Data.Maybe (fromMaybe)
 import qualified Data.Text as T
-import Data.Maybe (fromMaybe)
-
-import Network.Pocket.Types
+import           Formatting (sformat, string, (%))
+import           Network.Pocket.Types
+import           Network.Wreq (get, responseBody)
 
 fetchRedditCommentCount :: String -> String -> IO RedditCommentCount
 fetchRedditCommentCount subreddit articleId = do
   r <- get (T.unpack (sformat url subreddit articleId))
-  let n = r ^? responseBody . nth 0 . key "data" . key "children"  . nth 0 . key "data" . key "num_comments" . _Integer
+  let n = r ^? responseBody . nth 0 . key "data" . key "children" . nth 0 . key "data" . key "num_comments" . _Integer
   return (RedditCommentCount $ fromMaybe 0 n)
-  where url = "https://www.reddit.com/r/" % string % "/comments/" % string % ".json"
+  where
+    url = "https://www.reddit.com/r/" % string % "/comments/" % string % ".json"
